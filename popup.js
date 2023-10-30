3c3d80807b5a93adaacf63d1cc4aaf8031b0b2ab
jacky <jackywu526@gmail.com>
2023-10-30 15:25:22 +0800

    feat: 新增 補時數的功能

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var convertButton = document.getElementById('convertButton');
+  var workButton = document.getElementById('workButton');
   var resetButton = document.getElementById('resetButton');
   var homeTime = document.getElementById('homeTime');
   var homeTimeCountDown = document.getElementById('homeTimeCountDown');
+  /** 打卡時間 */
   var startTime = document.getElementById('startTime');
+  /**工作時數 */
   var workTime = document.getElementById('workTime');
+  /** 捕時數 */
+  var extraWorkTime = document.getElementById('extraWorkTime');
 
   // 取得儲存的設定
-  chrome.storage.sync.get(['startTime', 'workTime'], function(data) {
+  chrome.storage.sync.get(['startTime', 'workTime', 'extraWorkTime'], function(data) {
     if (data.startTime) {
       startTime.value = data.startTime;
       let getHour = ((startTime.value).split('.')[0]).split(':');
-      homeTime.textContent = `下班時間 ${Number(getHour[0]) + Number(workTime.value)} : ${getHour[1]} : ${getHour[2]}`;
+      homeTime.textContent = `下班時間 ${Number(getHour[0]) + Number(workTime.value) + Number(extraWorkTime.value)}  : ${getHour[1]} : ${getHour[2]}`;
       updateHomeTime();
     }
     if (data.workTime) {
@@ -19,26 +23,30 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  convertButton.addEventListener('click', function() {
+  // 開始計算工時和下班時間
+  workButton.addEventListener('click', function() {
     let getHour = ((startTime.value).split('.')[0]).split(':');
-    homeTime.textContent = `下班時間 ${Number(getHour[0]) + Number(workTime.value)} : ${getHour[1]} : ${getHour[2]}`;
+    homeTime.textContent = `下班時間 ${Number(getHour[0]) + Number(workTime.value) + Number(extraWorkTime.value)} : ${getHour[1]} : ${getHour[2]}`;
     updateHomeTime();
 
     // 儲存使用者的設定
     chrome.storage.sync.set({
       'startTime': startTime.value,
-      'workTime': workTime.value
+      'workTime': workTime.value,
+      'extraWorkTime': extraWorkTime.value
     });
   });
 
+  // init
   resetButton.addEventListener('click', function() {
     startTime.value = '';
     workTime.value = '9';
-
+    extraWorkTime.value = '0';
     // 清除儲存的設定
-    chrome.storage.sync.remove(['startTime', 'workTime']);
+    chrome.storage.sync.remove(['startTime', 'workTime', 'extraWorkTime']);
   });
 
+  // update 時間
   function updateHomeTime() {
     let now = new Date();
     let currentHours = now.getHours();
@@ -46,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentSeconds = now.getSeconds();
 
     let getHour = ((startTime.value).split('.')[0]).split(':');
-    let endHours = Number(getHour[0]) + Number(workTime.value);
+    let endHours = Number(getHour[0]) + Number(workTime.value) + Number(extraWorkTime.value);
     let endMinutes = Number(getHour[1]);
     let endSeconds = Number(getHour[2]);
 
@@ -66,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       homeTimeCountDown.textContent = `還剩下 ${diffHrs} 小時 ${diffMins} 分鐘 ${diffSecs} 秒`;
-      homeTime.textContent = `下班時間 ${Number(getHour[0]) + Number(workTime.value)} : ${getHour[1]} : ${getHour[2]}`;
+      homeTime.textContent = `下班時間 ${Number(getHour[0]) + Number(workTime.value) + Number(extraWorkTime.value)} : ${getHour[1]} : ${getHour[2]}`;
 
       // 每秒更新倒數時間
       setTimeout(updateHomeTime, 1000);
